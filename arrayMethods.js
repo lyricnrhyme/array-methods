6557ac8febad1c16bd14a3f10188a581ebb4a691
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-12 19:39:45 -1000

    push()

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,7 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+var nameArray = [];
+nameArray.push("Jamie");
+nameArray.push("Yamashita");
+genericNumberArray.push(6,7,8,9,10);
